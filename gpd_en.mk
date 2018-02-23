f0b1c32462df46997828c8c0751db7d69b964d82
Goayandi <r.borutha@gmail.com>
2018-02-23 22:14:50 +0100

    Update gpd_en.mk
    Fixing product name.

--- gpd_en.mk
+++ gpd_en.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/gpd/gpd_en/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gpd_en
-PRODUCT_NAME := GPD
+PRODUCT_NAME := gpd_en
 PRODUCT_BRAND := GPD
 PRODUCT_MANUFACTURER := GPD
 PRODUCT_MODEL := XD Plus

e5399fd133ff93c86dc7f9815c437b0730b0bfe3
Goayandi <r.borutha@gmail.com>
2018-02-23 22:15:33 +0100

    Update BoardConfig.mk
    Fixing kernel path.

--- gpd_en/BoardConfig.mk
+++ gpd_en/BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/xiaomi/cappu/kernel
+TARGET_PREBUILT_KERNEL := device/gpd/gpd_en/kernel
 BOARD_MTK_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_MKBOOTIMG_ARGS := --pagesize 2048 --base 0x40078000 --kernel_offset 0x00008000 --ramdisk_offset 0x08f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000
